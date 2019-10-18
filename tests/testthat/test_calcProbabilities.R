ff6411c417640b6b326102537372c015046b8fbd
Sarah Leavitt <sv1205@bu.edu>
2019-10-18 18:02:31 -0400

    Added my first unit test :)

---
+++ tests/testthat/test_calcProbabilities.R
@@ -0,0 +1,62 @@
+
+context("calcProbabilities Function")
+library(nbTransmission)
+
+## Use the pairData dataset which represents a TB-like outbreak
+# First create a dataset of ordered pairs
+orderedPair <- pairData[pairData$infectionDiffY > 0, ]
+
+## Create a variable called snpClose that will define probable links
+# (<3 SNPs) and nonlinks (>12 SNPs) all pairs with between 2-12 SNPs
+# will not be used to train.
+orderedPair$snpClose <- ifelse(orderedPair$snpDist < 3, TRUE,
+                               ifelse(orderedPair$snpDist > 12, FALSE, NA))
+table(orderedPair$snpClose)
+
+## Running the algorithm
+covariates = c("Z1", "Z2", "Z3", "Z4", "timeCat")
+resGen <- calcProbabilities(orderedPair = orderedPair, indIDVar = "individualID",
+                            edgeIDVar = "edgeID", goldStdVar = "snpClose",
+                            covariates = covariates, nReps = 1)
+
+
+test_that("calcProbabilities returns a list of two dataframes for valid input",{
+  expect_true(is.data.frame(resGen[[1]]))
+  expect_true(is.data.frame(resGen[[2]]))
+})
+
+
+test_that("Descriptive error messages returned",{
+  #Creating a function with defaults equal to my simulated data
+  calcProbWrapper <- function(orderedPair,
+                              indIDVar = "individualID",
+                              edgeIDVar = "edgeID",
+                              goldStdVar = "snpClose",
+                              covariates = c("Z1", "Z2", "Z3", "Z4", "timeCat"),
+                              nReps = 1){
+    
+    resGen <- calcProbabilities(orderedPair = orderedPair,
+                                indIDVar = indIDVar,
+                                edgeIDVar = edgeIDVar,
+                                goldStdVar = goldStdVar,
+                                covariates = c("Z1", "Z2", "Z3", "Z4", "timeCat"),
+                                nReps = nReps)
+    return(resGen)
+  }
+  
+ expect_error(calcProbWrapper(orderedPair, indIDVar = "garbage"),
+              "garbage.1 is not in the dataframe.")
+ 
+ #Removing individualID.2
+ orderedPair2 <- orderedPair %>% select(-individualID.2)
+ expect_error(calcProbWrapper(orderedPair2, indIDVar = "individualID"),
+              "individualID.2 is not in the dataframe.")
+ 
+ expect_error(calcProbWrapper(orderedPair, edgeIDVar = "garbage"),
+              "garbage is not in the dataframe.")
+ 
+ expect_error(calcProbWrapper(orderedPair, goldStdVar = "garbage"),
+              "garbage is not in the dataframe.")
+  
+})
+
